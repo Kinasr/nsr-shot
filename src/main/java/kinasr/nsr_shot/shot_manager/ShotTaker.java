e9b26fbeceecf2359143b532048ab4d849a64dc4
kinasr <kinasr95@gmail.com>
2023-10-14 20:50:46 +0300

    cleanup and document ShotTaker class

--- src/main/java/kinasr/nsr_shot/shot_manager/ShotTaker.java
+++ src/main/java/kinasr/nsr_shot/shot_manager/ShotTaker.java
@@ -8,11 +8,22 @@ import org.openqa.selenium.WebElement;
 public class ShotTaker {
     private ShotTaker() {
     }
-
+    /**
+     * Takes a full screenshot of the current web page.
+     *
+     * @param driver The WebDriver instance.
+     * @return The byte array representation of the screenshot.
+     */
     public static byte[] takeFullShot(WebDriver driver) {
         return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
     }
 
+    /**
+     * Takes a screenshot of a given web element.
+     *
+     * @param element The web element to capture the screenshot of.
+     * @return The screenshot of the web element as a byte array.
+     */
     public static byte[] takeElementShot(WebElement element) {
         return element.getScreenshotAs(OutputType.BYTES);
     }
