10c2d19838cb82e6443a97f0e889752108383129
kinasr <kinasr95@gmail.com>
2023-10-14 21:23:21 +0300

    cleanup and document TakeRef and TakeShot classes

--- src/main/java/kinasr/nsr_shot/shot_manager/TakeRef.java
+++ src/main/java/kinasr/nsr_shot/shot_manager/TakeRef.java
@@ -30,6 +30,13 @@ public class TakeRef {
         this.option = option;
     }
 
+    /**
+     * Adds a reference screenshot to the TakeShot object.
+     *
+     * @param referencePath the path to the reference screenshot image
+     * @return the updated TakeShot object with the reference screenshot added
+     * @throws IOException if there is an error reading the image file
+     */
     public TakeShot addRef(String referencePath) throws IOException {
         return new TakeShot(
                 driver,
@@ -41,30 +48,59 @@ public class TakeRef {
         );
     }
 
+    /**
+     * Takes a reference shot.
+     *
+     * @return The reference shot.
+     */
     public TakeShot takeRef() {
         setRefModelData(attribute.name());
 
         return refScreenshot(null);
     }
 
+    /**
+     * Takes a screenshot of the element located by the given By object.
+     *
+     * @param by the By object representing the element to be captured
+     * @return a TakeShot object containing the captured screenshot
+     */
     public TakeShot takeRef(By by) {
         setRefModelData(attribute.name());
 
         return refScreenshot(driver.findElement(by));
     }
 
+    /**
+     * Takes a reference screenshot of the specified web element.
+     * This method sets the reference model data and returns the reference screenshot.
+     *
+     * @param element The web element to take a reference screenshot of.
+     * @return The reference screenshot.
+     */
     public TakeShot takeRef(WebElement element) {
         setRefModelData(attribute.name());
 
         return refScreenshot(element);
     }
 
+    /**
+     * Sets the reference model data for a given name.
+     *
+     * @param name The name of the reference model.
+     */
     private void setRefModelData(String name) {
         ref.path(ConfigHandler.refDirectory())
                 .name(name)
                 .timestamp(REF_IMAGE_STAMP);
     }
 
+    /**
+     * Takes a screenshot of the given web element and returns a TakeShot object.
+     *
+     * @param element The web element to take a screenshot of. If null, takes a full page screenshot.
+     * @return The TakeShot object containing the screenshot and other information.
+     */
     private TakeShot refScreenshot(WebElement element) {
         hideUnwantedElements(driver, attribute.locators(), attribute.elements());
 
